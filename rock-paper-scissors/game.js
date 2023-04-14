ec0aca612bbc24539e65650e8a043d889e758134
killeraw50 <xxmarkc0xx@gmail.com>
2023-04-15 07:41:49 +0800

    working-rps

--- rock-paper-scissors/game.js
+++ rock-paper-scissors/game.js
@@ -1,38 +1,77 @@
-let rockPaperScissor = ["Rock", "Paper", "Scissors"];
-let randomNumber = Math.floor(Math.random() * 3);
-let computerSelection = rockPaperScissor[randomNumber];
+let rockPaperScissor = ["rock", "paper", "scissor"];
+let buttons = document.querySelectorAll("button");
+let playerScore = 0;
+let computerScore = 0;
+let rounds = 0;
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (rounds < 5) {
+            playerSelection = button.id;
+            let randomNumber = Math.floor(Math.random() * 3);
+            let computerSelection = rockPaperScissor[randomNumber];
+            fight(playerSelection, computerSelection);
+            document.querySelector("#player").textContent = "Player Score " + playerScore;
+            document.querySelector("#computer").textContent = "Computer Score " + computerScore;
+            rounds++;
+            if (rounds === 5) {
+                declareWinner();
+            }
+        }
+    })
+})
 function fight(playerSelection, computerSelection) {
-    if (playerSelection == "rock") {
-        if (computerSelection == playerSelection) {
+    if (playerSelection === "rock") {
+        if (computerSelection === playerSelection) {
             alert("Draw");
         }
-        else if (computerSelection == "paper") {
+        else if (computerSelection === "paper") {
             alert("Computer Wins");
+            computerScore++;
+
         }
         else {
             alert("Player Wins");
+            playerScore++;
         }
     }
-    else if (playerSelection == "paper") {
-        if (computerSelection == playerSelection) {
+    else if (playerSelection === "paper") {
+        if (computerSelection === playerSelection) {
             alert("Draw");
         }
-        else if (computerSelection == "rock") {
+        else if (computerSelection === "rock") {
             alert("Player Wins");
+            playerScore++;
         }
         else {
             alert("Computer Wins");
+            computerScore++;
         }
     }
-    else if (playerSelection == "scissors") {
-        if (computerSelection == playerSelection) {
+    else if (playerSelection === "scissor") {
+        if (computerSelection === playerSelection) {
             alert("Draw");
         }
-        else if (computerSelection == "rock") {
+        else if (computerSelection === "rock") {
             alert("Computer Wins");
+            computerScore++;
         }
         else {
             alert("Player Wins");
+            playerScore++;
         }
     }
+}
+function declareWinner() {
+    if (playerScore > computerScore) {
+        alert("Player Wins");
+    }
+    else if (playerScore < computerScore) {
+        alert("Computer Wins");
+    }
+    else {
+        alert("Draw");
+    }
+    playerScore = 0;
+    computerScore = 0;
+    rounds = 0;
 }
